f2aafcde6b472b6972e98723de1f4b46d4443bcc
nagle.zhang <nagle.zhang@sap.com>
2022-04-17 22:26:05 +0800

    prototype.

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# basic tools(git, curl)
+
+# k3s
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+kubectl create namespace jitsi
+
+# helm
+curl -sfL get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3  | bash -s -
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
+helm install myjitsi jitsi/jitsi-meet
+
