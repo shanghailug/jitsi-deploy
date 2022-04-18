14f621ddfabb73da0520359f1d5f7eeda51906b7
Nagle Zhang <nagle.zhang@qq.com>
2022-04-18 09:47:17 +0800

    using environment to make sure current setup apply to k3s.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # basic tools(git, curl)
-
-# k3s
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
-kubectl create namespace jitsi
+# k3s and set current context as k3s
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 && export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # helm
-curl -sfL get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3  | bash -s -
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
-helm install shlug-jitsi jitsi/jitsi-meet
-
+curl -sfL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3  | bash -s -
+#export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+sudo helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
+sudo helm install shlug-jitsi jitsi/jitsi-meet -f values.yml
