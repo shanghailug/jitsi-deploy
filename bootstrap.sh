12268ceac92f6be5d69cee02a2aea615aa9cce9a
Ubuntu <azureuser@vm.zqazjsiy4ukepgagcr5ofzezka.hx.internal.cloudapp.net>
2022-04-20 15:45:28 +0000

    disable traefik installation on k3s.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # k3s and set current context as k3s , k3s use trafik as ingress controller by default.
 (which k3s &> /dev/null && test -f /etc/rancher/k3s/k3s.yaml)  || \
-    curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+    curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --disable traefik
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl get namespace jitsi &> /dev/null || \
