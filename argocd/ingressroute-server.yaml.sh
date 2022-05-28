15f22dafad6f0df2b5c0958520eb09c419d3a05a
Wei He <hewei.com@gmail.com>
2022-05-28 15:24:53 +0800

    support manual cert

--- argocd/ingressroute-server.yaml.sh
+++ argocd/ingressroute-server.yaml.sh
@@ -1,3 +1,11 @@
+if [ -n "${FQDN}" ]; then
+  HOST_RULE="Host(\`${FQDN}\`)"
+  TLS_CERT_RESOLVER="certResolver: ${CERT_RESOLVER}"
+else
+  HOST_RULE="Host(\`${PUBLIC_IP}\`)"
+  TLS_MAP="{}"
+fi
+
 cat <<EOF
 apiVersion: traefik.containo.us/v1alpha1
 kind: IngressRoute
@@ -9,16 +17,16 @@ spec:
     - websecure
   routes:
     - kind: Rule
-      match: Host(\`${FQDN}\`) && PathPrefix(\`/argocd\`)
+      match: ${HOST_RULE} && PathPrefix(\`/argocd\`)
       services:
         - name: argocd-server
           port: 80
     - kind: Rule
-      match: Host(\`${FQDN}\`) && PathPrefix(\`/argocd\`) && Headers(\`Content-Type\`, \`application/grpc\`)
+      match: ${HOST_RULE} && PathPrefix(\`/argocd\`) && Headers(\`Content-Type\`, \`application/grpc\`)
       services:
         - name: argocd-server
           port: 80
           scheme: h2c
-  tls:
-    certResolver: ${CERT_RESOLVER}
+  tls: ${TLS_MAP}
+    ${TLS_CERT_RESOLVER}
 EOF
